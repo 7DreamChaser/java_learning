0508f49d99853fb1e1e95296d7735a0f9484fdcf
7DreamChaser <2729707823@qq.com>
2021-05-26 21:15:08 +0800

    hot-fix commit2

--- src/git_test.java
+++ src/git_test.java
@@ -1,6 +1,6 @@
 public class git_test {
     public static void main(String[] args) {
         System.out.println("first commit");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix(1)");
     }
 }
